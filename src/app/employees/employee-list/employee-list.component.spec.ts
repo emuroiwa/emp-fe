4e699dee711ea88d9eb5071298fc4979a940d2f2
muroiwa <emuroiwa@gmail.com>
2020-04-04 21:49:26 +0200

    employee-list component

--- src/app/employees/employee-list/employee-list.component.spec.ts
+++ src/app/employees/employee-list/employee-list.component.spec.ts
@@ -1,25 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/shared/employee.service';
+import { Employee } from 'src/app/shared/employee.model';
+//import { ToastrService } from 'ngx-toastr';
 
-import { EmployeeListComponent } from './employee-list.component';
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
 
-describe('EmployeeListComponent', () => {
-  let component: EmployeeListComponent;
-  let fixture: ComponentFixture<EmployeeListComponent>;
+  constructor(private service: EmployeeService) { }
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ EmployeeListComponent ]
-    })
-    .compileComponents();
-  }));
+  ngOnInit() {
+    this.service.refreshList();
+  }
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EmployeeListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  populateForm(emp: Employee) {
+    this.service.formData = Object.assign({}, emp);
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  onDelete(id: number) {
+    if (confirm('Are you sure to delete this record?')) {
+      this.service.deleteEmployee(id).subscribe(res => {
+        this.service.refreshList();
+      //  this.toastr.warning('Deleted successfully', 'EMP. Register');
+      });
+    }
+  }
+
+}
